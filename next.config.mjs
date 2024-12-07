30098dc449c04183f537f8b5186d036aaec6c258
antoniluthfi <luthfiantoni027@gmail.com>
2024-12-07 23:44:17 +0800

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,27 @@
 /** @type {import('next').NextConfig} */
+import path from "path";
+
+const __dirname = new URL(".", import.meta.url).pathname;
+
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'dummyimage.com',
+        protocol: "https",
+        hostname: "dummyimage.com",
       },
       {
-        protocol: 'https',
-        hostname: 'presensi.antonidev.com',
+        protocol: "https",
+        hostname: "presensi.antonidev.com",
       },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./"),
+    };
+    return config;
   },
 };
 
